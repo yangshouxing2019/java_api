8a71f45d4bb96f9aa43ac5cc366cd8f2f4db1f1f
yangshouxing2019 <2047898808@qq.com>
2019-11-24 16:11:27 +0800

    提交

--- java_api_object/src/main/java/com/tedu/cn/Hellow.java
+++ java_api_object/src/main/java/com/tedu/cn/Hellow.java
@@ -2,6 +2,8 @@ package com.tedu.cn;
 
 public class Hellow {
     public static void main(String[] args) {
-        System.out.println("degkerg");
+        System.out.println("*******");
+        System.out.println("*******");
+        System.out.println("*******");
     }
 }
