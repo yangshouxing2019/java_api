a5e4b6cc42713c1e8563ed36e13d4a2d19fb7f86
yangshouxing2019 <2047898808@qq.com>
2019-11-27 09:46:15 +0800

    提交

--- java_api_object/src/main/java/com/tedu/cn/Hellow.java
+++
@@ -1,8 +0,0 @@
-package com.tedu.cn;
-
-public class Hellow {
-    public static void main(String[] args) {
-        System.out.println("*******");
-        System.out.println("********");
-    }
-}
