451a5fe55f655bbe3315505ad50da0dec470a987
yangshouxing2019 <2047898808@qq.com>
2019-12-06 20:21:53 +0800

    jdk详情 杨宗保的50Java题 统计字符

---
+++ java_api_string/src/main/java/com/tedu/cn/PackageDemo.java
@@ -0,0 +1,12 @@
+package com.tedu.cn;
+
+import org.junit.Test;
+
+public class PackageDemo {
+    @Test
+    public  void  m1(){
+        Integer a=1;
+        System.out.println(Integer.toOctalString(12));
+
+    }
+}
